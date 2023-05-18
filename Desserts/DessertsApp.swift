d117f56e092f05b753998a01a20bd8d6ee8cc8e3
Abdallah Derbala <abdallahxderbala@gmail.com>
2023-05-18 08:19:19 -0400

    Initial Commit

---
+++ Desserts/DessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertsApp.swift
+//  Desserts
+//
+//  Created by Abdallah Derbala on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
