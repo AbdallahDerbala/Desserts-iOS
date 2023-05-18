ad1970669e7ae8068ba849bb59608943a7d557de
Abdallah Derbala <abdallahxderbala@gmail.com>
2023-05-18 08:45:42 -0400

    Set up async image view

--- Desserts/Views/AsyncImageView.swift
+++ Desserts/Views/AsyncImageView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct AsyncImageView: View {
+    @StateObject private var imageLoader = ImageLoader()
+    @Binding var urlString: String?
+    @State private var isVisible = false
+    
+    init(urlString: Binding<String?>) {
+        self._urlString = urlString
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        image
+            .onAppear {
+                isVisible.toggle()
+                loadImageIfNeeded(imageLoader: imageLoader, isVisible: isVisible, urlString: urlString)
+            }
+            .onDisappear {
+                isVisible.toggle()
+            }
+            .onChange(of: urlString) { _ in
+                loadImageIfNeeded(imageLoader: imageLoader, isVisible: isVisible, urlString: urlString)
+            }
+    }
+    
+    @ViewBuilder
+    private var image: some View {
+        if isVisible {
+            Group {
+                if let image = imageLoader.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Text("No Image")
+                }
+            }
+        } else {
+            Color.clear
+        }
     }
 }
 
+
 struct AsyncImageView_Previews: PreviewProvider {
+    
+    @State static var urlString: String? = dev.dessert?.imageURL
+    
     static var previews: some View {
-        AsyncImageView()
+        AsyncImageView(urlString: $urlString)
     }
 }
+
