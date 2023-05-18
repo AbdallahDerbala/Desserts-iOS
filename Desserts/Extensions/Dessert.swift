4705775df04e96b9dc3ca116241a791ccebf9848
Abdallah Derbala <abdallahxderbala@gmail.com>
2023-05-18 08:37:28 -0400

    Added extension for Dessert model

--- Desserts/Extensions/Dessert.swift
+++ Desserts/Extensions/Dessert.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+extension Dessert {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let mealDictionary = try container.decode([String: String?].self)
+        
+        var index = 1
+        var ingredients: [Ingredient] = []
+        
+        while
+            let ingredient = mealDictionary["strIngredient\(index)"] as? String,
+            let measure = mealDictionary["strMeasure\(index)"] as? String,
+            !ingredient.isEmpty, !measure.isEmpty
+        {
+            ingredients.append(.init(name: ingredient, measure: measure))
+            index += 1
+        }
+        self.ingredients = ingredients
+        id = mealDictionary["idMeal"] as? String ?? ""
+        name = mealDictionary["strMeal"] as? String ?? ""
+        imageURL = mealDictionary["strMealThumb"] as? String ?? ""
+        instructions = mealDictionary["strInstructions"] as? String ?? ""
+        youtubeURL = mealDictionary["strYoutube"] as? String ?? ""
+    }
+}
