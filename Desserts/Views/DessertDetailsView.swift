607dee49ad312a3c2dfe9bae9fd1e75f9fcbc491
Abdallah Derbala <abdallahxderbala@gmail.com>
2023-05-18 08:48:26 -0400

    Set up row and detail view for desserts

--- Desserts/Views/DessertDetailsView.swift
+++ Desserts/Views/DessertDetailsView.swift
@@ -8,13 +8,77 @@
 import SwiftUI
 
 struct DessertDetailsView: View {
+    @State var id: String
+    @StateObject private var mealGenerator: DessertDetailViewModel
+    
+    init(id: String) {
+        self._id = State(initialValue: id)
+        self._mealGenerator = StateObject(wrappedValue: DessertDetailViewModel(id: id))
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                if let name = mealGenerator.currentMeal?.name {
+                    HStack {
+                        Spacer()
+                        Text(name)
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                        Image(systemName: "heart.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 23)
+                            .foregroundColor(mealGenerator.currentMeal?.isFavorite() ?? false ? Color.pink : Color.black)
+                            .onTapGesture {
+                                mealGenerator.currentMeal?.toggleFavorite()
+                            }
+                    }
+                    .onAppear{ mealGenerator.currentMeal?.setRecent() }
+                }
+                AsyncImageView(urlString: $mealGenerator.currentImageURL)
+                
+                if mealGenerator.currentMeal?.youtubeURL.isEmpty ?? true { } else {
+                    if let youtubeURL = URL(string: mealGenerator.currentMeal?.youtubeURL ?? "") {
+                        Link("Youtube Video", destination: youtubeURL)
+                    }
+                }
+                
+                
+                
+                
+                if let ingredients = mealGenerator.currentMeal?.ingredients {
+                    HStack {
+                        Text("Ingredients")
+                            .font(.title2)
+                            .padding(.top)
+                        Spacer()
+                    }
+                    ForEach(ingredients, id: \.self) { ingredient in
+                        HStack {
+                            Text(ingredient.name + " - " + ingredient.measure)
+                            Spacer()
+                        }
+                    }
+                }
+                if let instructions = mealGenerator.currentMeal?.instructions {
+                    HStack {
+                        Text("Instructions")
+                            .font(.title2)
+                            .padding(.top)
+                        Spacer()
+                    }
+                    Text(instructions)
+                }
+            }
+            .padding()
+        }
     }
 }
 
 struct DessertDetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        DessertDetailsView()
+        DessertDetailsView(id: "53049")
     }
 }
--- Desserts/Views/DessertRowView.swift
+++ Desserts/Views/DessertRowView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct DessertRowView: View {
+    
+    @State var id: String
+    @StateObject private var mealGenerator: DessertDetailViewModel
+    
+    init(id: String) {
+        self._id = State(initialValue: id)
+        self._mealGenerator = StateObject(wrappedValue: DessertDetailViewModel(id: id))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            AsyncImageView(urlString: $mealGenerator.currentImageURL)
+                .cornerRadius(10)
+            
+            Text(mealGenerator.currentMeal?.name ?? "")
+                .font(.title)
+                .fontWeight(.light)
+            Spacer(minLength: 0)
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 13)
+                .fill(Color.clear)
+        )
+        .frame(height: 55)
+        .background(
+            NavigationLink("", destination: DessertDetailsView(id: id)).opacity(0)
+        )
     }
 }
 
 struct DessertRowView_Previews: PreviewProvider {
     static var previews: some View {
-        DessertRowView()
+        DessertRowView(id: "53049")
     }
 }
